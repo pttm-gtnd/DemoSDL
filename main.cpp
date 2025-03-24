a519ecede26216ad8b7216317e8a4bd7f8057fa2
pttm-gtnd <phamthitramy1306@gmail.com>
2025-03-24 21:01:46 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -68,6 +68,88 @@ public:
     }
 };
 
+class EnemyTank {
+public:
+    int x, y;
+    int dirX, dirY;
+    int moveDelay, shootDelay;
+    SDL_Rect rect;
+    bool active;
+    vector<Bullet> bullets;
+
+    EnemyTank(int startX, int startY) {
+        moveDelay = 15;
+        shootDelay = 5;
+        x = startX;
+        y = startY;
+        rect = {x, y, TILE_SIZE, TILE_SIZE};
+        dirX = 0;
+        dirY = 1;
+        active = true;
+    }
+    void move(const vector<Wall>& walls) {
+        if (--moveDelay > 0) return;
+        moveDelay = 15;
+        int r = rand() % 4;
+        if (r == 0) {
+            this->dirX = 0;
+            this->dirY = -5;
+        }
+        else if (r == 1) {
+            this->dirX = 0;
+            this->dirY = 5;
+        }
+        else if (r == 2) {
+            this->dirX = 0;
+            this->dirY = -5;
+        }
+        else if (r == 3) {
+            this->dirX = 0;
+            this->dirY = 5;
+        }
+        int newX = x + this->dirX;
+        int newY = y + this->dirY;
+
+        SDL_Rect newRect = { newX, newY, TILE_SIZE, TILE_SIZE };
+        for (const auto& wall : walls) {
+            if (wall.active && SDL_HasIntersection(&newRect, &wall.rect)) {
+                return;
+            }
+        }
+
+        if (newX >= TILE_SIZE && newX <= SCREEN_WIDTH - TILE_SIZE * 2 &&
+            newY >= TILE_SIZE && newY <= SCREEN_HEIGHT - TILE_SIZE * 2) {
+                x = newX;
+                y = newY;
+                rect.x = x;
+                rect.y = y;
+            }
+    }
+
+    void shoot() {
+        if (--shootDelay > 0) return;
+        shootDelay = 5;
+        bullets.push_back(Bullet(x + TILE_SIZE / 2 - 5, y + TILE_SIZE / 2 - 5,
+                                 this->dirX, this->dirY));
+    }
+
+    void updateBullets() {
+        for (auto &bullet : bullets) {
+            bullet.move();
+        }
+        bullets.erase(std::remove_if(bullets.begin(), bullets.end(),
+                                     [](Bullet &b) {return !b.active;}), bullets.end());
+    }
+
+    void render(SDL_Renderer* renderer) {
+        SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
+        SDL_RenderFillRect(renderer, &rect);
+        for (auto &bullet : bullets) {
+            bullet.render(renderer);
+        }
+    }
+};
+
 class PlayerTank {
 public:
     int x, y;
@@ -134,6 +216,8 @@ public:
     bool running;
     vector<Wall> walls;
     PlayerTank player;
+    int enemyNumber = 3;
+    vector<EnemyTank> enemies;
 
     Game() : player(((MAP_WIDTH -1) / 2) * TILE_SIZE, (MAP_HEIGHT - 2) * TILE_SIZE) {
         running = true;
